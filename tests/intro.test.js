0243194583a8bcf89c9fd79aefdf0bb5bffd903a
Piyu0423 <piumikamadubashini5@gmail.com>
2025-01-28 10:37:41 +0800

    simple test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,19 @@
 import { describe, test, it, expect } from "vitest";
+import { max } from "../src/intro";
 
 describe("max", () => {
-  it("should return the first argument if it is greater", () => {});
+  it("should return the first argument if it is greater", () => {
+    //Arrange
+    const a = 2;
+    const b = 1;
+
+    //AAA
+    const results = max(a, b);
+
+    //Assert
+    expect(results).toBe(2);
+
+    // using a single line
+    // expect(max(a, b)).toBe(2);
+  });
 });
