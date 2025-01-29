5719af046345ead53c8a058d1b87a2182b2b43ba
Piyu0423 <piumikamadubashini5@gmail.com>
2025-01-29 12:16:05 +0800

    test cases for calculating average

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max, fizzBuzz } from "../src/intro";
+import { max, fizzBuzz, calculateAverage, factorial } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -7,7 +7,7 @@ describe("max", () => {
     // const a = 2;
     // const b = 1;
 
-    //AAA
+    //Act
     // const results = max(a, b);
 
     //Assert
@@ -36,5 +36,20 @@ describe("fizzBuzz", () => {
   });
   it("should return number if it is not divisible by 5 or 3", () => {
     expect(fizzBuzz(8)).toBe("8");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("should return NaN if given an empty array", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+  it("should calculate the average of an array with single element", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+  it("should calculate the average of an array with two element", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+  it("should calculate the average of an array with three element", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
   });
 });
